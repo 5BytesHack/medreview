56634a67174f08c4ef468a999938c6130a20bf75
AlaverArt <AlaverArt@yandex.ru>
2022-04-08 22:04:18 +0300

    Перед слиянием с Диминой веткой

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,35 +1,37 @@
 <template>
   <div class="">
-    <!--<nav class="nav-header d-flex justify-content-between">
-      <router-link to="/"><img src="@/img/logo_white.png" alt="user_icon">MedReview</router-link>
-      <router-link to="/about"></router-link>
-    </nav>-->
-    <nav class="navbar navbar-expand-lg">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <a class="navbar-brand" href="/">Navbar</a>
 
-        <div class="">
-          <a class="navbar-brand" href="#!"><img src="@/img/logo_white.png" alt="user_icon">MedReview</a>
-        </div>
-
-        <div class="collapse navbar-collapse justify-content-end" id="navbarTogglerDemo02">
-          <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon">ggg</span>
-          </button>
-          <ul class="navbar-nav mr-auto mt-2 mt-md-0 ">
-            <li class="nav-item active">
-              <a class="nav-link" href="#!">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#!">Link</a>
-            </li>
-          </ul>
-        </div>
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul class="navbar-nav mr-auto mt-2 mt-md-0">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Личный кабинет<span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/about">О нас</a>
+          </li>
+        </ul>
+      </div>
     </nav>
-
     <router-view/>
   </div>
 </template>
 
-<style>
+<script>
+export default {
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
+
+<style scoped>
 @import'bootstrap/dist/css/bootstrap.min.css';
 
 .nav-header{
