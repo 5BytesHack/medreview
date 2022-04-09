c006b02a6f875d1ba75aa8f82a44c030e6e86f2e
through-your-tears <warstream55@gmail.com>
2022-04-09 10:47:12 +0300

    Добавлен бэкенд классификатора

---
+++ backend/classifier/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import *
+
+app_name = 'classifier'
+urlpatterns = [
+    path('manage/', ManagerReviewAPIView.as_view),
+    path('allreviews/', AllReviewsAPIView.as_view),
+    path('user/reviews/', UserReviewsAPIView.as_view),
+    path('review/<int:id>', ReviewAPIView.as_view)
+]
