b10e4d3461b5853e489fc715d4e822ce7fe45c7e
Арсений Сергеевич Белоусов <s0153348@edu.kubsu.ru>
2022-04-10 14:31:44 +0300

    commented in digitalization and fixed and checked classier

--- digitalization/digitalizer.py
+++ digitalization/digitalizer.py
@@ -3,16 +3,17 @@ import urllib
 import easyocr
 import PIL
 import urllib.request
-url = 'https://avatars.mds.yandex.net/get-zen_doc/927575/pub_5ae4d1d6c3321b1f23ef2094_5ae4d2739e29a23794d8eab0/scale_1200'
-img = urllib.request.urlopen(url).read()
-out = open("img.jpg", "wb")
-out.write(img)
-out.close
+# url = 'https://avatars.mds.yandex.net/get-zen_doc/927575/pub_5ae4d1d6c3321b1f23ef2094_5ae4d2739e29a23794d8eab0/
+# scale_1200'
+# img = urllib.request.urlopen(url).read()
+# out = open("img.jpg", "wb")
+# out.write(img)
+# out.close
 
 # import library
 
 
-#specify shortform of language you want to extract,
+# specify shortform of language you want to extract,
 # I am using Hindi(hi) and English(en) here by list of language ids
 
 reader = 'null'
